4fe2633d0d90f3956df368545e65830dbd1cc5c1
Santi <Santi@DESKTOP-IG9DTLS>
2025-05-07 11:52:55 -0300

    Connect to Database

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import { connectDB } from "./config/database.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const APP = express();
 const PORT = 3000;
@@ -7,5 +11,8 @@ APP.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 APP.listen(PORT, () => {
+  connectDB()
+    .then(() => console.log("Database connected successfully"))
+    .catch((error) => console.error("Database connection error:", error));
   console.log(`Server is running on port: http://localhost:${PORT}`);
 });
